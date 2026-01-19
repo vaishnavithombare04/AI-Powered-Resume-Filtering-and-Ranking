f64c9ed18e2ee883acb7a599248e090a4e9f94ee
vaishnavithombare04 <thovaishnavi513@gmail.com>
2026-01-19 20:52:19 +0530

    Update __init__.py

--- Resume_Parser/__init__.py
+++ Resume_Parser/__init__.py
@@ -1,19 +1,1 @@
-import pdfplumber
 
-def extract_text_from_pdf(pdf_path):
-    text = ""
-    with pdfplumber.open(pdf_path) as pdf:
-        for page in pdf.pages:
-            page_text = page.extract_text()
-            if page_text:
-                text += page_text + "\n"
-    return text
-
-pdf_path = "resumes.pdf"   
-resume_text = extract_text_from_pdf(pdf_path)
-
-
-with open("resume_text.txt", "w", encoding="utf-8") as file:
-    file.write(resume_text)
-
-print("Resume text successfully saved to requirements.txt")
